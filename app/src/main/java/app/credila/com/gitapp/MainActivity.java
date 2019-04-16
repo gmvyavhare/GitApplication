a574eb77ab44bf91c93dd61dc6794c7e6c07c6b9
GaneshVyavhare <ganeshvyavhare13@gmail.com>
2019-04-16 11:47:56 +0530

    GitSample hello World ok

--- app/src/main/java/app/credila/com/gitapp/MainActivity.java
+++ app/src/main/java/app/credila/com/gitapp/MainActivity.java
@@ -9,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //This is sample only.
         System.out.println("Hello World");
+
+
     }
 }
